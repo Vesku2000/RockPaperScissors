0beaeeed194b204889825d847046f81571939935
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2022-10-04 13:23:09 +0300

    challenges done

--- loops2.js
+++ loops2.js
@@ -92,9 +92,55 @@ for(let x = 0; x < 5; x++){
 }
 console.log(numExist.length);
 //10. Luo ohjelma joka generoi 8 merkkiä pitkän satunnaisia merkkejä sisältävän tunnuksen, esim. 57MGS8F1
-//
+function generatePassword(passwordLength) {
+    var numberChars = "0123456789";
+    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    var allChars = numberChars + upperChars + lowerChars;
+    var randPasswordArray = Array(passwordLength);
+    randPasswordArray[0] = numberChars;
+    randPasswordArray[1] = upperChars;
+    randPasswordArray[2] = lowerChars;
+    randPasswordArray = randPasswordArray.fill(allChars, 3);
+    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+  }
+  
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+  
+  console.log(generatePassword(8));
 //11. Luo ohjelma joka generoi sattumanvaraisen värin rgb-arvona. Esim. rgb(240,180,80)
-//
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+  
+
+    console.log(getRandomRgb());
+  
 //12. Luo ohjelma joka ottaa vastaan alla olevan taulukon ja tulostaa "NASA"
-//
+const minorPlanets = [
+    "Newtonia",
+    "Arecibo",
+    "Sibelius",
+    "Ariane"
+]
+
+
+    
+    for(const brand of minorPlanets){
+        console.log(brand[0]);
+        
+    }
+
 //const minorPlanets = ["Newtonia", "Arecibo", "Sibelius", "Ariane"];
